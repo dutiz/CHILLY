21a2a819c898a8e51e85402c4917f7c114fd96b7
dutiz <zaimidaut@gmail.com>
2023-12-01 14:18:06 +0100

    update

--- components/ContactFormCard.js
+++ components/ContactFormCard.js
@@ -3,6 +3,21 @@
 import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 export default function ContactFormCard({ t }) {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const myForm = event.target
+    const formData = new FormData(myForm)
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => navigate('/success/'))
+      .catch((error) => alert(error))
+  }
+
   return (
     <div className="lg:col-5 col-10 mt-8 lg:mt-0 lg:text-start text-center">
       <AnimationOnScroll animateIn="animate__fadeInRight" delay={500}>
@@ -10,8 +25,8 @@ export default function ContactFormCard({ t }) {
           {t('formTitle')}
         </p>
         <h1 className="mt-5 text-4xl text-gray-700  font-bold">{t('formSubtitle')}</h1>
-        <form name="contact" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
+        <form data-netlify="true" name="contactForm" method="post" onSubmit={handleSubmit}>
+              <input type="hidden" name="form-name" value="contactForm" />
           <div className="row mt-10">
             <div className="lg:col-6">
               <input
@@ -55,7 +70,7 @@ export default function ContactFormCard({ t }) {
                 placeholder={t('message')}
                 className="mt-8 p-5 border-2 border-yellow-400 rounded-3xl w-full placeholder-black font-bold"
               />
-              <button type="submit" className="mt-10 bg-yellow-400 px-10 rounded-3xl py-5">
+              <input type="submit" className="mt-10 bg-yellow-400 px-10 rounded-3xl py-5">
                 {t('buttonMessage')}
               </button>
             </div>
